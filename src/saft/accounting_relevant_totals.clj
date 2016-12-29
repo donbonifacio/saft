65ec7ff7f348b72b56756f503406c7b35204e66b
Pedro Santos <donbonifacio@gmail.com>
2016-12-29 11:40:24 +0000

    Add comments and tests for accounting relevant totals

--- src/saft/accounting_relevant_totals.clj
+++ src/saft/accounting_relevant_totals.clj
@@ -1,10 +1,13 @@
-(ns saft.accounting-relevant-totals
+(ns ^{:added "0.1.0" :author "Pedro Pereira Santos"}
+  saft.accounting-relevant-totals
+  "Loads and writes accountint relevant totals."
   (:require
     [clojure.data.xml :as xml]
     [clojure.java.jdbc :as j]
     [saft.common :as common]))
 
 (defn run
+  "Performs que query to obtain the accounting relevant totals."
   [db {:keys [begin end account]}]
   (common/query-time-info "[SQL] Accouting relevant totals query"
     (let [not_canceled (str "(invoices.status <> 'canceled')")
@@ -26,7 +29,9 @@
           result (first (j/query db [sql]))]
       result)))
 
-(defn totals-xml [totals]
+(defn totals-xml
+  "Given the totals, writes the xml for them."
+  [totals]
   [(xml/element :NumberOfEntries {} (:number_of_entries totals))
    (xml/element :TotalDebit {} (:total_debit totals))
    (xml/element :TotalCredit {} (:total_credit totals))])
--- test/saft/accounting_relevant_totals_test.clj
+++ test/saft/accounting_relevant_totals_test.clj
@@ -1,3 +1,15 @@
 (ns saft.accounting-relevant-totals-test
   (:require [clojure.test :refer :all]
+            [clojure.data.xml :as xml]
             [saft.accounting-relevant-totals :as accounting-relevant-totals]))
+
+(deftest totals-xml-test
+  (is (= (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+              "<NumberOfEntries>10</NumberOfEntries>"
+              "<TotalDebit>10.4</TotalDebit>"
+              "<TotalCredit>5.88</TotalCredit>")
+         (-> {:number_of_entries 10
+              :total_debit 10.4
+              :total_credit 5.88}
+             (accounting-relevant-totals/totals-xml)
+             (xml/emit-str)))))
