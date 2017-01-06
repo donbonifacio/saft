a94ba7804d5c37976bce1e6879854061ab9ac460
Pedro Santos <donbonifacio@gmail.com>
2017-01-06 11:10:04 +0000

    Add docs and tests for common

--- src/saft/common.clj
+++ src/saft/common.clj
@@ -1,11 +1,14 @@
-(ns saft.common
+(ns ^{:added "0.1.0" :author "Pedro Pereira Santos"}
+  saft.common
   (:require
     [clojure.data.xml :as xml]
     [clj-time.format :as f]
     [clj-time.coerce :as c]
     [clj-time.core :as t]))
 
-(def unknown "Desconhecido")
+(def unknown
+  "This represents the unknown string, in Portuguese."
+  "Desconhecido")
 
 (defn get-str
   "Gets and prepares a string value from a map"
@@ -20,6 +23,8 @@
        (apply str (take size (filter #(<= 32 (int %)) value)))))))
 
 (defmacro time-info
+  "This is a copy of the time macro of std clojure. It adds an addition
+  string label and outputs it."
   [info expr]
   `(let [start# (. System (nanoTime))
          ret# ~expr]
@@ -27,6 +32,9 @@
      ret#))
 
 (defmacro query-time-info
+  "This is a copy of the time macro of std clojure. It adds an addition
+  string label and outputs it. It assumes that the result of expr will be
+  a collection and outputs it's size."
   [info expr]
   `(let [start# (. System (nanoTime))
          ret# ~expr]
@@ -35,88 +43,139 @@
                    "(loaded " (if (map? ret#) 1 (count ret#)) " rows)"))
      ret#))
 
-(defn debit-documents [account]
-  ["Invoice" "CashInvoice" "DebitNote" "InvoiceReceipt" "SimplifiedInvoice"])
+(defn debit-documents
+  "All the possible debit documents."
+  ([]
+   (debit-documents nil))
+  ([account]
+   ["Invoice" "CashInvoice" "DebitNote" "InvoiceReceipt" "SimplifiedInvoice"]))
 
-(defn credit-documents [account]
-  ["CreditNote"])
+(defn credit-documents
+  "All the possible credit documents."
+  ([]
+   (credit-documents nil))
+  ([account]
+   ["CreditNote"]))
 
 (defn guide-documents
+  "All the possible guide documents."
   ([]
    (guide-documents nil))
   ([account]
    ["Shipping" "Devolution" "Transport"]))
 
-(defn guide? [type-name]
-  (first (filter #(= type-name %) (guide-documents))))
+(defn guide?
+  "Given a type name, returns true if the type is a Guide."
+  [type-name]
+  (boolean (first (filter #(= type-name %) (guide-documents)))))
 
-(defn payment-documents [account]
-  ["Receipt"])
+(defn payment-documents
+  "All the possible payment documents."
+  ([]
+   (payment-documents nil))
+  ([account]
+   ["Receipt"]))
 
-(defn statuses-relevant-for-communication []
+(defn statuses-relevant-for-communication
+  "The list of statuses that should be considered for communication."
+  []
   ["sent" "settled" "second_copy" "canceled"])
 
-(defn invoice? [doc-type]
+(defn invoice?
+  "True if the document type is an invoice."
+  [doc-type]
   (= "Invoice" doc-type))
 
-(defn db-string-coll [coll]
+(defn db-string-coll
+  "Given a coll of strings, returns each element surrounded by '.
+  Removes Invoice, because this is to be used on queries and Invoice
+  maps to null."
+  [coll]
   (->> coll
        (remove invoice?)
        (map #(str "'" %  "'"))))
 
 (defn saft-status-str
+  "Returns the saft status as a db usable string."
   []
   (clojure.string/join ","
     (db-string-coll (statuses-relevant-for-communication))))
 
-(defn types-condition [types]
+(defn types-condition
+  "Given a coll of type names, returns the query part for them."
+  [types]
   (let [db-types (db-string-coll types)]
     (str "(" (when (first (filter invoice? types))
                 "invoices.type is null or ")
          "invoices.type in (" (clojure.string/join "," db-types) "))")))
 
-(defn saft-types-condition [account]
+(defn saft-types-condition
+  "The condition for the types that make sense on saft."
+  [account]
   (types-condition
      (concat (debit-documents account)
              (credit-documents account))))
 
-(defn saft-guides-condition [account]
+(defn saft-guides-condition
+  "The condition for the guide types for saft."
+  [account]
   (types-condition
      (guide-documents account)))
 
-(defn all-types-condition [account]
+(defn all-types-condition
+  "A condition for all types that could be on a saft."
+  [account]
   (types-condition
      (concat (debit-documents account)
              (credit-documents account)
              (payment-documents account)
              (guide-documents account))))
 
-(def date-formatter (f/formatter "yyyy-MM-dd"))
-(def saft-formatter (f/formatters :date-hour-minute-second))
+(def date-formatter
+  "This is the simple Date format for saft."
+  (f/formatter "yyyy-MM-dd"))
 
-(defn generated-date []
-  (f/unparse date-formatter (t/now)))
+(def saft-formatter
+  "This is the simple DateTime format for saft"
+  (f/formatters :date-hour-minute-second))
 
-(defn fiscal-year [date]
+(defn generated-date 
+  "Outputs the generated date string for saft."
+  ([]
+   (generated-date (t/now)))
+  ([datetime]
+   (f/unparse date-formatter datetime)))
+
+(defn fiscal-year
+  "Gets the year of a date."
+  [date]
   (t/year (f/parse date-formatter date)))
 
-(defn saft-date [date]
+(defn saft-date
+  "Gets a proper date string from the arg. Arg can be string or an sql date"
+  [date]
   (if (string? date)
     date
     (f/unparse saft-formatter (c/from-sql-date date))))
 
-(defn month [date]
+(defn month
+  "Gets the month of a date."
+  [date]
   (cond
     (string? date) (t/month (f/parse date))
     :else (t/month (c/from-sql-date date))))
 
-(defn get-date [m k]
+(defn get-date
+  "Given a key and a map, fetches the value and returns it as a SAFT date."
+  [m k]
   (let [date (get m k)]
     (cond
       (string? date) (f/unparse date-formatter (f/parse date))
       :else (f/unparse date-formatter (c/from-sql-date date)))))
 
-(defn get-date-time [m k]
+(defn get-date-time
+  "Given a key and a map, fetches the value and returns it as a SAFT datetime."
+  [m k]
   (let [date (get m k)]
     (cond
       (string? date) (f/unparse saft-formatter (f/parse date))
--- test/saft/common_test.clj
+++ test/saft/common_test.clj
@@ -1,6 +1,27 @@
 (ns saft.common-test
   (:require [clojure.test :refer :all]
+            [clj-time.core :as t]
             [saft.common :as common]))
+
+(deftest get-str-test
+  (is (= "Hello" (common/get-str {:k "Hello"} :k)))
+  (is (= "He" (common/get-str {:k "Hello"} :k 2)))
+  (is (= "Hello" (common/get-str {} :k 2 "Hello")))
+  (is (= "Hello" (common/get-str {:k ""} :k 2 "Hello")))
+  (is (= common/unknown (common/get-str {} :k 2))))
+
+(deftest guide?-test
+  (is (true? (common/guide? "Shipping")))
+  (is (true? (common/guide? "Transport")))
+  (is (true? (common/guide? "Devolution")))
+  (is (false? (common/guide? "Invoice")))
+  (is (false? (common/guide? "CreditNote")))
+  (doseq [guide-name (common/guide-documents)]
+    (is (true? (common/guide? guide-name))))
+  (doseq [doc-name (concat (common/debit-documents)
+                           (common/credit-documents)
+                           (common/payment-documents))]
+    (is (false? (common/guide? doc-name)))))
 
 (deftest types-condition-test
   (is (= "(invoices.type is null or invoices.type in ('InvoiceReceipt'))"
@@ -9,3 +30,47 @@
          (common/types-condition ["CreditNote"])))
   (is (= "(invoices.type in ('DebitNote','SimplifiedInvoice'))"
          (common/types-condition ["DebitNote" "SimplifiedInvoice"]))))
+
+(deftest db-string-coll-test
+  (is (= ["'CreditNote'"] (common/db-string-coll ["CreditNote" "Invoice"])))
+  (is (= ["'CreditNote'"] (common/db-string-coll ["CreditNote"])))
+  (is (= ["'CreditNote'", "'DebitNote'"] (common/db-string-coll ["CreditNote" "DebitNote"]))))
+
+(deftest saft-status-str-test
+  (is (= "'sent','settled','second_copy','canceled'"
+         (common/saft-status-str))))
+
+(deftest types-condition-test
+  (is (= "(invoices.type is null or invoices.type in ('CreditNote','DebitNote'))"
+         (common/types-condition ["Invoice"  "CreditNote" "DebitNote"])))
+  (is (= "(invoices.type in ('CreditNote','DebitNote'))"
+         (common/types-condition ["CreditNote" "DebitNote"]))))
+
+(deftest saft-types-condition-test
+  (is (= "(invoices.type is null or invoices.type in ('CashInvoice','DebitNote','InvoiceReceipt','SimplifiedInvoice','CreditNote'))"
+         (common/saft-types-condition {}))))
+
+(deftest saft-guides-condition-test
+  (is (= "(invoices.type in ('Shipping','Devolution','Transport'))"
+         (common/saft-guides-condition {}))))
+
+(deftest all-types-condition-test
+  (is (= "(invoices.type is null or invoices.type in ('CashInvoice','DebitNote','InvoiceReceipt','SimplifiedInvoice','CreditNote','Receipt','Shipping','Devolution','Transport'))"
+         (common/all-types-condition {}))))
+
+(deftest generated-date-test
+  (is (= "2017-01-01" (common/generated-date (t/date-time 2017 1 1)))))
+
+(deftest fiscal-year-test
+  (is (= 2017 (common/fiscal-year "2017-01-01"))))
+
+(deftest month-test
+  (is (= 1 (common/month "2017-01-01"))))
+
+(deftest get-date-test
+  (is (= "2017-01-01"
+         (common/get-date {:date "2017-01-01"} :date))))
+
+(deftest get-date-time-test
+  (is (= "2017-01-01T00:00:00"
+         (common/get-date-time {:date "2017-01-01"} :date))))
