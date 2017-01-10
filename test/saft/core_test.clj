73c5587c7ae54239d14cc1db7224d7837f6d4cab
Pedro Santos <donbonifacio@gmail.com>
2017-01-10 17:27:55 +0000

    By default, don't run database test

--- test/saft/core_test.clj
+++ test/saft/core_test.clj
@@ -4,7 +4,7 @@
             [saft.core :as core]))
 
 (deftest local-db-test
-  (core/generate-saft {:account-id 5554 #_6599
+  #_(core/generate-saft {:account-id 5554 #_6599
                        :formatted true
                        ;:begin "2012-01-01" :end "2012-12-31"
                        :begin "2016-01-01" :end "2016-12-31"
